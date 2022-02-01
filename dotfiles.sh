4eba9c21ef838b344b7a795aa233b3ca10e06b6a
Thomas Bentham <thomascbentham@gmail.com>
2022-02-01 18:48:45 +1100

    added install scripts

---
+++ dotfiles.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+VSCODE_EXTENSIONS_PATH="vscode_extensions.txt"
+
+
+help(){
+    printf "Dotfile installer and vscode extension tools\n\n";
+    printf "Syntax: ./dotfiles.sh [l|load|s|save|i|install]\n\n";
+    printf "Options:\n\n";
+    printf "l|load      loads the vscode extensions from: %s\n" $VSCODE_EXTENSIONS_PATH;
+    printf "s|save      saves the current vscode extensions into: %s\n" $VSCODE_EXTENSIONS_PATH;
+    printf "i|install   installs the current dotfiles\n";
+    printf "\n";
+}
+
+
+exit_abnormal(){
+    help;
+    exit 1;
+}
+
+
+load(){
+    while read extension; do
+        code --install-extension $extension --force
+    done < $VSCODE_EXTENSIONS_PATH;
+    return;
+}
+
+
+save(){
+    printf "Saving extensions to: %s" $VSCODE_EXTENSIONS_PATH;
+    code --list-extensions > $VSCODE_EXTENSIONS_PATH;
+    return;
+}
+
+
+install(){
+    DOTFOLDERS=(
+        "fish"
+        "nvim"
+        "vscode"
+        "tmux"
+    )
+    printf "Stowing Folders: \n"
+    for folder in "${DOTFOLDERS[@]}"
+    do
+        printf "\t%s\n" $folder
+        stow --target $HOME $folder --override=man --override=info
+    done
+    printf "done"
+    return;
+}
+
+
+while getopts lhsi-: OPT; do
+    if [ "$OPT" = "-" ]; then
+        OPT="${OPTARG%%=*}"
+        OPTARG="${OPTARG#$OPT}"
+        OPTARG="${OPTARG#=}"
+    fi
+    case "$OPT" in
+        l | load    ) load ;;
+        h | help    ) help ;;
+        s | save    ) save ;;
+        i | install ) install;;
+        ??*         ) exit_abnormal ;;
+        ?           ) exit_abnormal ;;
+    esac
+    exit;
+done
+
+exit 1;
